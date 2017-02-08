7a4a970c5a2afca00dee2f1f8036eb7f13322088
Md.Mahafuzur Rahaman <1420224@iub.edu.bd>
2017-02-07 19:44:08 -0800

    Update tictactoe.js

--- tictactoe.js
+++ tictactoe.js
@@ -1,1 +1,16 @@
 //JS code goes here
+//JS code goes here
+var turn =true;
+var clicked=[];
+function play(event){
+  var element=event.target;
+  if(!clicked[element.id])
+  {
+    clicked[element.id]=true;
+    console.log(clicked);
+      console.log(event);
+    element.innerHTML=turn?"X":"0";
+    turn=!turn;
+  }
+
+}
